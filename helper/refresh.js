a943ad970d123f77366acf20b218af54fbdce1cd
Nino <ninofocus@gmail.com>
2014-12-13 16:28:59 +0800

    submodule use http protocol

--- helper/refresh.js
+++ helper/refresh.js
@@ -4,8 +4,14 @@ var
 
 function refresh() {
     var command = 'git pull origin masterl && git submodule foreach git pull origin master && pm2 restart ndoc';
+
     return exec(command).then(function(std) {
         var stdout = std[0], stderr = std[1];
+        if(stderr) {
+            console.log(stderr);
+        } else {
+            console.log(stdout);
+        }
     });
 }
 
