e7bd02e9a63a6c1431fb45565bae97d01d2a07e0
Nino <ninofocus@gmail.com>
2014-12-13 16:52:49 +0800

    fix command error

--- helper/refresh.js
+++ helper/refresh.js
@@ -3,16 +3,15 @@ var
     exec = Q.denodeify(require('child_process').exec);
 
 function refresh() {
-    var command = 'git pull origin masterl && git submodule foreach git pull origin master && pm2 restart ndoc';
+    var command = 'git pull origin master && git submodule foreach git pull origin master && pm2 restart ndoc';
 
-    return exec(command).then(function(std) {
-        var stdout = std[0], stderr = std[1];
-        if(stderr) {
-            console.log(stderr);
-        } else {
-            console.log(stdout);
-        }
-    });
+    return exec(command)
+        .then(function(std) {
+            var stdout = std[0], stderr = std[1];
+            console.log(stderr ? stderr : stdout);
+        }).fail(function(error) {
+            console.log(error);
+        });
 }
 
 module.exports = refresh;
