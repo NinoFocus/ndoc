271286493046758584f7f6e86e7a97fcb0aa1285
Nino <ninofocus@gmail.com>
2014-12-13 23:38:53 +0800

    add duoshuo

--- public/javascripts/docs.js
+++ public/javascripts/docs.js
@@ -2,3 +2,17 @@ $(function() {
     // highlight
     hljs.initHighlightingOnLoad();
 });
+
+$(function() {
+    if($('.ds-thread').length === 0) return;
+
+    window.duoshuoQuery = {short_name:"ninotips"};
+    (function() {
+        var ds = document.createElement('script');
+        ds.type = 'text/javascript';ds.async = true;
+        ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.unstable.js';
+        ds.charset = 'UTF-8';
+        (document.getElementsByTagName('head')[0]
+         || document.getElementsByTagName('body')[0]).appendChild(ds);
+    })();
+})
